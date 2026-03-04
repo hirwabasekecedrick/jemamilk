a5791d161ca99dcebf6747beaba0b53e24f39938
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-03-04 15:56:08 -0500

    Adding Login Frontend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,34 +1,12 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { LoginForm } from "@/components/login-form";
+import { ModeToggle } from "@/components/theme-toggle";
 
-export default function Auth(){
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-
-    const handleSubmit = async (e : React.FormEvent) => {
-        e.preventDefault();
-
-        const res = await fetch("api/login",{
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email,password})
-        })
-
-        if (res.ok) {
-            const data = await res.json();
-            return console.log("User Login Succesful",data);
-            
-        }else{
-            return console.log("Failed To log in ")
-        }
-    }
-    return(
-        <>
-        <form onSubmit={handleSubmit}>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" onChange={(e) => setPassword(e.target.value)} />
-            <input type="submit" value="Login" />
-        </form>
-        </>
-    )
+export default function Auth() {
+  return (
+    <>
+    <ModeToggle/>
+      <LoginForm />
+    </>
+  );
 }
