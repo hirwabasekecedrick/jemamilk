8a565224310d94c3aff0a83911b349cf83a71b22
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-03-04 15:43:54 -0500

    Removing Build Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
 
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
