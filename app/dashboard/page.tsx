63977999880c8b66690e5555199d56e157709ad0
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-03-08 15:53:51 -0400

    Adding the REport , stock and hr folder structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to Dashboard</h1>
-      <p className="mt-4 text-gray-600">You are successfully logged in</p>
     </div>
   );
 }
